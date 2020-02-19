d8284b5bf67275d259c92ee35fe6a49d5f51c61e
ISHOXRUZ <48910512+ISHOXRUZ@users.noreply.github.com>
2020-02-19 03:48:33 -0800

    MainTAbBarController

--- IMUSIC/MainTabBarController.swift
+++ IMUSIC/MainTabBarController.swift
@@ -13,9 +13,28 @@ class MainTabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        view.backgroundColor = .gray
+        view.backgroundColor = .white
+        
+        tabBar.tintColor = #colorLiteral(red: 1, green: 0, blue: 0.3764705882, alpha: 1)
+        
+        
+        viewControllers = [
+            generateViewController(rootViewController: SearchTableViewController(), image: #imageLiteral(resourceName: "search"), title: "Search"),
+            generateViewController(rootViewController: ViewController(), image: #imageLiteral(resourceName: "library"), title: "Library")
+        ]
         
 
+    }
+    private func generateViewController(rootViewController: UIViewController, image: UIImage, title: String) ->UIViewController {
+        
+        let navigationVC = UINavigationController(rootViewController: rootViewController)
+        navigationVC.tabBarItem.image = image
+        navigationVC.tabBarItem.title = title
+        rootViewController.navigationItem.title = title
+        navigationVC.navigationBar.prefersLargeTitles = true
+        
+        return navigationVC
+        
     }
     
 }
