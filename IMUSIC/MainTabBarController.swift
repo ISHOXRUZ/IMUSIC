0739d9712770278c9c58705140350df1b6d68219
ISHOXRUZ <48910512+ISHOXRUZ@users.noreply.github.com>
2020-02-19 02:54:33 -0800

    First

---
+++ IMUSIC/MainTabBarController.swift
@@ -0,0 +1,21 @@
+//
+//  MainTabBarController.swift
+//  IMUSIC
+//
+//  Created by Lucifer on 2/19/20.
+//  Copyright © 2020 Lucifer. All rights reserved.
+//
+
+import UIKit
+
+class MainTabBarController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        view.backgroundColor = .gray
+        
+
+    }
+    
+}
